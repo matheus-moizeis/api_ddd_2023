5d7d013062e05571d64a8b6ef3603a22f14ebee0
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-02-27 23:42:19 -0300

    finalizando aula 2

---
+++ src/Entities/Entities/Notifies.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entities
+{
+    public class Notifies
+    {
+        [NotMapped]
+        public string NomePropriedade { get; set; }
+
+        [NotMapped]
+        public string mensagem { get; set; }
+
+        [NotMapped]
+        public List<Notifies> Notificacoes { get; set; }
+
+        public Notifies()
+        {
+            Notificacoes = new List<Notifies>();
+        }
+
+        public bool ValidarPropriedadeString(string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+        public bool ValidarPropriedadeInt(int valor, string nomePropriedade)
+        {
+            if (valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                Notificacoes.Add(new Notifies
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade
+                });
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/Entities/Enums/TipoUsuario.cs
@@ -0,0 +1,8 @@
+﻿namespace Entities.Entities.Enums
+{
+    public enum TipoUsuario
+    {
+        Administrador = 1,
+        Comun = 2,
+    }
+}
