6244bf384802bd54fa6ac3bcfc9d27f8628e2cae
Matheus Moizeis <matheusmoizeis@outlook.com>
2023-03-17 00:27:04 -0300

    finalizando parte 4

---
+++ src/Api/Models/Login.cs
@@ -0,0 +1,9 @@
+namespace Api.Models
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+    }
+}
---
+++ src/Api/Models/MessageViewModel.cs
@@ -0,0 +1,16 @@
+namespace Api.Models
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataAlteracao { get; set; }
+        public string UserId { get; set; }
+    }
+}
